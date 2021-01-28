372c80ad4d4c1307965fadbd4e2da2157628e01c
HODONG <hodong@gmail.com>
2021-01-28 17:34:01 +0900

    load data from db, can add object to db

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,18 +1,39 @@
-from flask import Flask, render_template
-import psycopg2
+from flask import Flask, render_template, request
+import psycopg2, ast
 app = Flask(__name__)
 
-@app.route('/')
+def db():
+    conn = psycopg2.connect(dbname="dev", user = "awsuser", password = "aws!Red1",
+                        host = "redshift-cluster-1.crrx4sdg4mct.ap-northeast-2.redshift.amazonaws.com", port = "5439")
+    return conn
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        cur = db().cursor()
+        # Executing a SQL query
+        cur.execute("SELECT * from public.todolist")
+        todoList = cur.fetchall()
+        print(todoList)
+        return render_template('index.html', todoList = todoList)
+    elif request.method == "POST":
+        print("post", request.data.decode('UTF-8'))
+        mydata = ast.literal_eval(request.data.decode('UTF-8'))
+        print(mydata['element'])
+
+        conn = db()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO public.todolist(user_id, todo) VALUES ('1', %(obj)s);", {"obj": mydata['element']})
+        cur.close()
+        conn.commit()
+        conn.close()
+
+        return render_template('index.html')
 
 @app.route('/get')
 def list():
-    conn = psycopg2.connect(dbname="dev", user = "awsuser", password = "aws!Red1",
-                            host = "redshift-cluster-1.crrx4sdg4mct.ap-northeast-2.redshift.amazonaws.com", port = "5439")
-    cur = conn.cursor()
+    cur = db().cursor()
     # Executing a SQL query
-    cur.execute("SELECT * from public.todo")
+    cur.execute("SELECT * from public.todolist")
     todoList = cur.fetchall()
     print(todoList)
     return render_template('index.html', todoList = todoList)
